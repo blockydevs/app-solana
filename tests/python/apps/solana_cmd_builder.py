97f069039b7b56fa94196010116afaa2f0ac7a11
Maciej Malik <maciej.malik@silksh.pl>
2023-11-10 13:16:12 +0100

    review fixes

--- tests/python/apps/solana_cmd_builder.py
+++ tests/python/apps/solana_cmd_builder.py
@@ -1,12 +1,10 @@
-from enum import IntEnum
 from typing import List
-
+from enum import IntEnum
 import base58
 from nacl.signing import VerifyKey
 
-PROGRAM_ID_SYSTEM = "11111111111111111111111111111111"
 
-PROGRAM_ID_COMPUTE_BUDGET = "ComputeBudget111111111111111111111111111111"
+PROGRAM_ID_SYSTEM = "11111111111111111111111111111111"
 
 # Fake blockhash so this example doesn't need a network connection. It should be queried from the cluster in normal use.
 FAKE_RECENT_BLOCKHASH = "11111111111111111111111111111111"
@@ -17,12 +15,6 @@ def verify_signature(from_public_key: bytes, message: bytes, signature: bytes):
     verify_key = VerifyKey(from_public_key)
     verify_key.verify(message, signature)
 
-
-class ComputeBudgetInstruction(IntEnum):
-    RequestUnits        = 0x00
-    RequestHeapFrame    = 0x01
-    SetComputeUnitLimit = 0x02
-    SetComputeUnitPrice = 0x03
 
 class SystemInstruction(IntEnum):
     CreateAccount           = 0x00
@@ -51,7 +43,6 @@ class MessageHeader:
                self.num_readonly_signed_accounts.to_bytes(1, byteorder='little') + \
                self.num_readonly_unsigned_accounts.to_bytes(1, byteorder='little')
 
-
 class AccountMeta:
     pubkey: bytes
     is_signer: bool
@@ -61,7 +52,6 @@ class AccountMeta:
         self.is_signer = is_signer
         self.is_writable = is_writable
 
-
 # Only support Transfer instruction for now
 # TODO add other instructions if the need arises
 class Instruction:
@@ -70,62 +60,14 @@ class Instruction:
     data: bytes
     from_pubkey: bytes
     to_pubkey: bytes
-    num_required_signatures: int
-    num_readonly_signed_accounts: int
-    num_readonly_unsigned_accounts: int
-
-
-class ComputeBudgetInstructionRequestUnits(Instruction):
-    def __init__(self, units: int, additionalFee: int):
-        self.program_id = base58.b58decode(PROGRAM_ID_COMPUTE_BUDGET)
-        self.accounts = []
-        self.data = ((ComputeBudgetInstruction.RequestUnits).to_bytes(1, byteorder='little')
-                     + (additionalFee).to_bytes(4, byteorder='little')
-                     + (units).to_bytes(4, byteorder='little'))
-
-
-class ComputeBudgetRequestHeapFrame(Instruction):
-    def __init__(self, bytes: int):
-        self.program_id = base58.b58decode(PROGRAM_ID_COMPUTE_BUDGET)
-        self.accounts = []
-        self.data = (ComputeBudgetInstruction.RequestHeapFrame).to_bytes(1, byteorder='little') + (bytes).to_bytes(4, byteorder='little')
-
-
-class ComputeBudgetInstructionUnitLimit(Instruction):
-    def __init__(self, lamports_amount: int):
-        self.from_pubkey = bytearray()
-        self.to_pubkey = bytearray()
-        self.num_required_signatures = 0
-        self.num_readonly_signed_accounts = 0
-        self.num_readonly_unsigned_accounts = 1
-        self.program_id = base58.b58decode(PROGRAM_ID_COMPUTE_BUDGET)
-        self.accounts = []
-        self.data = (ComputeBudgetInstruction.SetComputeUnitLimit).to_bytes(1, byteorder='little') + (lamports_amount).to_bytes(4, byteorder='little')
-
-
-class ComputeBudgetSetComputeUnitPrice(Instruction):
-    def __init__(self, lamports_amount: int):
-        self.from_pubkey = bytearray()
-        self.to_pubkey = bytearray()
-        self.num_required_signatures = 0
-        self.num_readonly_signed_accounts = 0
-        self.num_readonly_unsigned_accounts = 1
-        self.program_id = base58.b58decode(PROGRAM_ID_COMPUTE_BUDGET)
-        self.accounts = []
-        self.data = (ComputeBudgetInstruction.SetComputeUnitPrice).to_bytes(1, byteorder='little') + (lamports_amount).to_bytes(4, byteorder='little')
-
 
 class SystemInstructionTransfer(Instruction):
     def __init__(self, from_pubkey: bytes, to_pubkey: bytes, amount: int):
-        self.num_required_signatures = 2
-        self.num_readonly_signed_accounts = 0
-        self.num_readonly_unsigned_accounts = 1
         self.from_pubkey = from_pubkey
         self.to_pubkey = to_pubkey
         self.program_id = base58.b58decode(PROGRAM_ID_SYSTEM)
         self.accounts = [AccountMeta(from_pubkey, True, True), AccountMeta(to_pubkey, False, True)]
         self.data = (SystemInstruction.Transfer).to_bytes(4, byteorder='little') + (amount).to_bytes(8, byteorder='little')
-
 
 # Cheat as we only support 1 SystemInstructionTransfer currently
 # TODO add support for multiple transfers and other instructions if the needs arises
@@ -143,55 +85,26 @@ class CompiledInstruction:
         serialized: bytes = self.program_id_index.to_bytes(1, byteorder='little')
         serialized += len(self.accounts).to_bytes(1, byteorder='little')
         for account in self.accounts:
-            serialized += account.to_bytes(1, byteorder='little')
+            serialized += (account).to_bytes(1, byteorder='little')
         serialized += len(self.data).to_bytes(1, byteorder='little')
         serialized += self.data
         return serialized
 
-
-# Solana's communication message, header + list of public keys used by the instructions + instructions
+# Solana communication message, header + list of public keys used by the instructions + instructions
 # with references to the keys array
 class Message:
     header: MessageHeader
-    account_keys: List[bytes]
+    account_keys: List[AccountMeta]
     recent_blockhash: bytes
     compiled_instructions: List[CompiledInstruction]
 
     def __init__(self, instructions: List[Instruction]):
+        # Cheat as we only support 1 SystemInstructionTransfer currently
+        # TODO add support for multiple transfers and other instructions if the needs arises
+        self.header = MessageHeader(2, 0, 1)
+        self.account_keys = [instructions[0].from_pubkey, instructions[0].to_pubkey, instructions[0].program_id]
         self.recent_blockhash = base58.b58decode(FAKE_RECENT_BLOCKHASH)
-
-        tmp_num_required_signatures = 0
-        tmp_num_readonly_signed_accounts = 0
-        tmp_num_readonly_unsigned_accounts = 0
-
-        self.account_keys = []
-        writable_accounts = set()
-        readonly_accounts = set()
-
-        self.compiled_instructions = []
-
-        # Parse accounts
-        for instruction in instructions:
-            readonly_accounts.add(instruction.program_id)
-            tmp_num_required_signatures += instruction.num_required_signatures
-            tmp_num_readonly_signed_accounts += instruction.num_readonly_signed_accounts
-            tmp_num_readonly_unsigned_accounts += instruction.num_readonly_unsigned_accounts
-            for account in instruction.accounts:
-                if account.is_writable:
-                    writable_accounts.add(account.pubkey)
-
-        self.account_keys += writable_accounts
-        self.account_keys += readonly_accounts
-
-        # Prepare instructions
-        for instruction in instructions:
-            account_indexes = []
-            for account in instruction.accounts:
-                if account.is_writable:
-                    account_indexes += [self.account_keys.index(account.pubkey)]
-            self.compiled_instructions += [CompiledInstruction(self.account_keys.index(instruction.program_id), account_indexes, instruction.data)]
-
-        self.header = MessageHeader(tmp_num_required_signatures, tmp_num_readonly_signed_accounts, tmp_num_readonly_unsigned_accounts)
+        self.compiled_instructions = [CompiledInstruction(2, [0, 1], instructions[0].data)]
 
     def serialize(self) -> bytes:
         serialized: bytes = self.header.serialize()
@@ -200,10 +113,8 @@ class Message:
             serialized += account_key
         serialized += self.recent_blockhash
         serialized += len(self.compiled_instructions).to_bytes(1, byteorder='little')
-        for instruction in self.compiled_instructions:
-            serialized += instruction.serialize()
+        serialized += self.compiled_instructions[0].serialize()
         return serialized
-
 
 def is_printable_ascii(string: str) -> bool:
     try:
@@ -211,7 +122,6 @@ def is_printable_ascii(string: str) -> bool:
         return True
     except UnicodeDecodeError:
         return False
-
 
 def is_utf8(string: str) -> bool:
     return True
@@ -232,12 +142,10 @@ MAX_LEN: int = U16_MAX - BASE_HEADER_LEN - MESSAGE_HEADER_LEN;
 # Max Length of the OffchainMessage supported by the Ledger
 MAX_LEN_LEDGER: int = PACKET_DATA_SIZE - BASE_HEADER_LEN - MESSAGE_HEADER_LEN;
 
-
 class MessageFormat(IntEnum):
     RestrictedAscii = 0x00
     LimitedUtf8     = 0x01
     ExtendedUtf8    = 0x02
-
 
 class v0_OffchainMessage:
     format: MessageFormat
--- tests/python/test_solana.py
+++ tests/python/test_solana.py
@@ -1,13 +1,10 @@
 from ragger.backend import RaisePolicy
 from ragger.navigator import NavInsID, NavIns
 from ragger.utils import RAPDU
-import base58
 
 from .apps.solana import SolanaClient, ErrorType
-from .apps.solana_cmd_builder import SystemInstructionTransfer, Message, verify_signature, OffchainMessage, \
-    ComputeBudgetInstructionUnitLimit, ComputeBudgetSetComputeUnitPrice
-from .apps.solana_utils import FOREIGN_PUBLIC_KEY, FOREIGN_PUBLIC_KEY_2, AMOUNT, AMOUNT_2, SOL_PACKED_DERIVATION_PATH, \
-    SOL_PACKED_DERIVATION_PATH_2, ROOT_SCREENSHOT_PATH, COMPUTE_BUDGET_UNIT_LIMIT, COMPUTE_BUDGET_UNIT_PRICE
+from .apps.solana_cmd_builder import SystemInstructionTransfer, Message, verify_signature, OffchainMessage
+from .apps.solana_utils import FOREIGN_PUBLIC_KEY, FOREIGN_PUBLIC_KEY_2, AMOUNT, AMOUNT_2, SOL_PACKED_DERIVATION_PATH, SOL_PACKED_DERIVATION_PATH_2, ROOT_SCREENSHOT_PATH
 from .apps.solana_utils import enable_blind_signing, enable_short_public_key, enable_expert_mode, navigation_helper_confirm, navigation_helper_reject
 
 
@@ -36,6 +33,7 @@ class TestGetPublicKey:
 
         assert sol.get_async_response().data == from_public_key
 
+
     def test_solana_get_public_key_refused(self, backend, navigator, test_name):
         sol = SolanaClient(backend)
         with sol.send_public_key_with_confirm(SOL_PACKED_DERIVATION_PATH):
@@ -59,94 +57,6 @@ class TestGetPublicKey:
         assert sol.get_async_response().status == ErrorType.USER_CANCEL
 
 
-class TestComputeBudgetTransfer:
-
-    def test_solana_transfer_with_compute_budget_limit_ok_1(self, backend, navigator, test_name):
-        sol = SolanaClient(backend)
-        from_public_key = sol.get_public_key(SOL_PACKED_DERIVATION_PATH)
-
-        # Create instruction
-        instruction_unit_limit = ComputeBudgetInstructionUnitLimit(COMPUTE_BUDGET_UNIT_LIMIT)
-        instruction: SystemInstructionTransfer = SystemInstructionTransfer(from_public_key, FOREIGN_PUBLIC_KEY, AMOUNT)
-        message: bytes = Message([instruction_unit_limit, instruction]).serialize()
-
-        # Displayed data and transfer amount should not change
-        with sol.send_async_sign_message(SOL_PACKED_DERIVATION_PATH, message):
-            navigation_helper_confirm(navigator, backend.firmware.device, test_name)
-
-        signature: bytes = sol.get_async_response().data
-        verify_signature(from_public_key, message, signature)
-
-    def test_solana_transfer_with_compute_budget_limit_refused_1(self, backend, navigator, test_name):
-        sol = SolanaClient(backend)
-        from_public_key = sol.get_public_key(SOL_PACKED_DERIVATION_PATH)
-
-        # Create instruction
-        instruction_unit_limit = ComputeBudgetInstructionUnitLimit(COMPUTE_BUDGET_UNIT_LIMIT)
-        instruction_unit_price = ComputeBudgetSetComputeUnitPrice(COMPUTE_BUDGET_UNIT_PRICE)
-        instruction: SystemInstructionTransfer = SystemInstructionTransfer(from_public_key, FOREIGN_PUBLIC_KEY, AMOUNT)
-        message: bytes = Message([instruction_unit_limit, instruction]).serialize()
-
-        backend.raise_policy = RaisePolicy.RAISE_NOTHING
-        with sol.send_async_sign_message(SOL_PACKED_DERIVATION_PATH, message):
-            navigation_helper_reject(navigator, backend.firmware.device, test_name)
-
-        rapdu: RAPDU = sol.get_async_response()
-        assert rapdu.status == ErrorType.USER_CANCEL
-
-    def test_solana_transfer_with_compute_budget_limit_and_unit_price_refused_1(self, backend, navigator, test_name):
-        sol = SolanaClient(backend)
-        from_public_key = sol.get_public_key(SOL_PACKED_DERIVATION_PATH)
-
-        # Create instruction
-        instruction_unit_limit = ComputeBudgetInstructionUnitLimit(COMPUTE_BUDGET_UNIT_LIMIT)
-        instruction_unit_price = ComputeBudgetSetComputeUnitPrice(COMPUTE_BUDGET_UNIT_PRICE)
-        instruction: SystemInstructionTransfer = SystemInstructionTransfer(from_public_key, FOREIGN_PUBLIC_KEY, AMOUNT)
-        message: bytes = Message([instruction_unit_price, instruction_unit_limit, instruction]).serialize()
-
-        backend.raise_policy = RaisePolicy.RAISE_NOTHING
-        with sol.send_async_sign_message(SOL_PACKED_DERIVATION_PATH, message):
-            navigation_helper_reject(navigator, backend.firmware.device, test_name)
-
-        rapdu: RAPDU = sol.get_async_response()
-        assert rapdu.status == ErrorType.USER_CANCEL
-
-    def test_solana_transfer_with_compute_budget_limit_and_unit_price_ok_1(self, backend, navigator, test_name):
-        sol = SolanaClient(backend)
-        from_public_key = sol.get_public_key(SOL_PACKED_DERIVATION_PATH)
-
-        # Create instruction
-        instruction_unit_limit = ComputeBudgetInstructionUnitLimit(COMPUTE_BUDGET_UNIT_LIMIT)
-        instruction_unit_price = ComputeBudgetSetComputeUnitPrice(COMPUTE_BUDGET_UNIT_PRICE)
-        instruction: SystemInstructionTransfer = SystemInstructionTransfer(from_public_key, FOREIGN_PUBLIC_KEY, AMOUNT)
-        message: bytes = Message([instruction_unit_price, instruction_unit_limit, instruction]).serialize()
-
-        # Displayed data and transfer amount should not change
-        with sol.send_async_sign_message(SOL_PACKED_DERIVATION_PATH, message):
-            navigation_helper_confirm(navigator, backend.firmware.device, test_name)
-
-        signature: bytes = sol.get_async_response().data
-        verify_signature(from_public_key, message, signature)
-
-    def test_solana_transfer_with_compute_budget_limit_and_unit_price_ok_2(self, backend, navigator, test_name):
-        sol = SolanaClient(backend)
-        from_public_key = sol.get_public_key(SOL_PACKED_DERIVATION_PATH)
-
-        # Create instruction
-        instruction_unit_limit = ComputeBudgetInstructionUnitLimit(230000)
-        instruction_unit_price = ComputeBudgetSetComputeUnitPrice(12546)
-        # Prioritization fee should be equal to 2886 lamports
-        instruction: SystemInstructionTransfer = SystemInstructionTransfer(from_public_key, FOREIGN_PUBLIC_KEY, AMOUNT)
-        message: bytes = Message([instruction_unit_price, instruction_unit_limit, instruction]).serialize()
-
-        # Displayed data and transfer amount should not change
-        with sol.send_async_sign_message(SOL_PACKED_DERIVATION_PATH, message):
-            navigation_helper_confirm(navigator, backend.firmware.device, test_name)
-
-        signature: bytes = sol.get_async_response().data
-        verify_signature(from_public_key, message, signature)
-
-
 class TestMessageSigning:
 
     def test_solana_simple_transfer_ok_1(self, backend, navigator, test_name):
@@ -163,6 +73,7 @@ class TestMessageSigning:
         signature: bytes = sol.get_async_response().data
         verify_signature(from_public_key, message, signature)
 
+
     def test_solana_simple_transfer_ok_2(self, backend, navigator, test_name):
         sol = SolanaClient(backend)
         from_public_key = sol.get_public_key(SOL_PACKED_DERIVATION_PATH_2)
@@ -176,6 +87,7 @@ class TestMessageSigning:
 
         signature: bytes = sol.get_async_response().data
         verify_signature(from_public_key, message, signature)
+
 
     def test_solana_simple_transfer_refused(self, backend, navigator, test_name):
         sol = SolanaClient(backend)
@@ -207,6 +119,7 @@ class TestOffchainMessageSigning:
         signature: bytes = sol.get_async_response().data
         verify_signature(from_public_key, message, signature)
 
+
     def test_ledger_sign_offchain_message_ascii_refused(self, backend, navigator, test_name):
         sol = SolanaClient(backend)
         from_public_key = sol.get_public_key(SOL_PACKED_DERIVATION_PATH)
@@ -220,6 +133,7 @@ class TestOffchainMessageSigning:
 
         rapdu: RAPDU = sol.get_async_response()
         assert rapdu.status == ErrorType.USER_CANCEL
+
 
     def test_ledger_sign_offchain_message_ascii_expert_ok(self, backend, navigator, test_name):
         enable_expert_mode(navigator, backend.firmware.device, test_name + "_1")
@@ -235,6 +149,7 @@ class TestOffchainMessageSigning:
 
         signature: bytes = sol.get_async_response().data
         verify_signature(from_public_key, message, signature)
+
 
     def test_ledger_sign_offchain_message_ascii_expert_refused(self, backend, navigator, test_name):
         enable_expert_mode(navigator, backend.firmware.device, test_name + "_1")
@@ -252,6 +167,7 @@ class TestOffchainMessageSigning:
         rapdu: RAPDU = sol.get_async_response()
         assert rapdu.status == ErrorType.USER_CANCEL
 
+
     def test_ledger_sign_offchain_message_utf8_ok(self, backend, navigator, test_name):
         enable_blind_signing(navigator, backend.firmware.device, test_name + "_1")
 
@@ -266,6 +182,7 @@ class TestOffchainMessageSigning:
 
         signature: bytes = sol.get_async_response().data
         verify_signature(from_public_key, message, signature)
+
 
     def test_ledger_sign_offchain_message_utf8_refused(self, backend, navigator, test_name):
         enable_blind_signing(navigator, backend.firmware.device, test_name + "_1")
@@ -283,6 +200,7 @@ class TestOffchainMessageSigning:
         rapdu: RAPDU = sol.get_async_response()
         assert rapdu.status == ErrorType.USER_CANCEL
 
+
     def test_ledger_sign_offchain_message_utf8_expert_ok(self, backend, navigator, test_name):
         enable_blind_signing(navigator, backend.firmware.device, test_name + "_1")
         enable_expert_mode(navigator, backend.firmware.device, test_name + "_2")
@@ -298,6 +216,7 @@ class TestOffchainMessageSigning:
 
         signature: bytes = sol.get_async_response().data
         verify_signature(from_public_key, message, signature)
+
 
     def test_ledger_sign_offchain_message_utf8_expert_refused(self, backend, navigator, test_name):
         enable_blind_signing(navigator, backend.firmware.device, test_name + "_1")
